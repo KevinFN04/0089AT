788d493311903b6bd06d41ee1c33e7e7ff0faf63
KevinFN04 <kevin.felix.04@gmail.com>
2016-03-04 12:55:17 +0100

    Creado el constructor de la clase AgendaTelefonica

--- AgendaTelefonica.java
+++ AgendaTelefonica.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /**
  * Write a description of class agendaTelefonica here.
  * 
@@ -7,12 +7,14 @@
  */
 public class AgendaTelefonica
 {
+    Map<String, String> agendaTlf;
     
     /**
      * Constructor for objects of class agendaTelefonica
      */
     public AgendaTelefonica()
     {
-
+        agendaTlf = new HashMap<String, String>();
     }
+
 }
